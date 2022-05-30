96b7980ea732447f16db0b482ee57ce6771d2ced
Green_Lab <greencoder001@outlook.com>
2022-05-30 14:52:40 +0200

    OS Info

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const os = require('os')
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Cyclic seems to be awesome!'
-    })
+    res.send(JSON.stringify({
+        totalRAM: os.totalmem(),
+        freeRAM: os.freemem(),
+        hostname: os.hostname(),
+        platform: os.platform(),
+        release: os.release(),
+        type: os.type(),
+        uptime: os.uptime(),
+        loadAvg: os.loadavg(),
+        networkInterfaces: os.networkInterfaces(),
+        userInfo: os.userInfo()
+    }, null, 2))
 })
 
 app.listen(8080)
